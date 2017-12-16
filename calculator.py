ec890d75f1a01e4cdd4ad2bf88d50089c10b9eed
hades_zhao <hades_zhao@126.com>
2017-12-16 21:14:38 +0800

    finished

--- calculator.py
+++ calculator.py
@@ -86,34 +86,58 @@ class Argument(object):
         arg_index =arglist.index(self._arg)
         return arglist[arg_index + 1]
 
+que1 = Queue()
+que2 = Queue()
 
+def putdata(arg):
+     _argument = Argument(arg)
+     quelist =[(k,v) for k, v in\
+     UserData(_argument.ne_arg).\
+     userdata.items()]
+     que1.put(quelist)
+
+def comp_func(soinsurp,basel,baseh,lock):
+    bftaxlist = que1.get()
+    g = (bftaxlist)
+    for i in g:
+         bftax = i[1]
+         with lock:
+              salary = Salary(bftax,soinsurp,basel,baseh)
+              sal_list = [i[0],salary._bftax,salary.soinsur,salary.pitax,\
+                        salary.aftax]
+              que2.put(sal_list)
+
+def outfile(arg):
+    sal_list = que2.get() 
+    _argument = Argument(arg)
+    with open(_argument.ne_arg,'a') as file:
+         file.write(sal_list[0])
+         for i in sal_list[1:]:
+             file.write(','+'{:.2f}'.format(i)) 
 if __name__ == '__main__': 
-    argc = Argument('-c')
-    config_argc = Config(argc.ne_arg)
-    soinsurp = config_argc.config['ShengYu'] + config_argc.config['YangLao'] + config_argc.config['YiLiao'] + config_argc.config['GongJiJin'] + config_argc.config['GongShang'] + config_argc.config['ShiYe']
-    basel = config_argc.config['JiShuL']
-    baseh = config_argc.config['JiShuH']
-    #
-    que1 = Queue() 
-    que2 = Queue()
-    def putdata(arg):
-        _argument = Argument(arg)
-        quelist =[(k,v) for k, v in UserData(_argument.ne_arg).userdata.items()]
-        que1.put(quelist)
-    def comp_func():
-        bftaxlist = que1.get()
-        g = (bftaxlist)
-        sal_list = []
-        for i in g:
-            bftax = i[1]
-            salary = Salary(bftax,soinsurp,basel,baseh)
-            sal_tuple = (i[0],salary._bftax,salary.soinsur,salary.pitax,\
-                        salary.aftax)
-            sal_list.append(sal_tuple)
-        que2.put(sal_list)
+    try:
+        arglist = sys.argv[1:]
+        if len(arglist) ==6 and '-c' in arglist and\
+        '-d' in arglist and '-o'in arglist
+            argc = Argument('-c')
+            config_argc = Config(argc.ne_arg)
+            soinsurp = config_argc.config['ShengYu']\ 
+               + config_argc.config['YangLao'] \
+               + config_argc.config['YiLiao'] \
+               + config_argc.config['GongJiJin']\  
+               + config_argc.config['GongShang'] \
+               + config_argc.config['ShiYe']
+            basel = config_argc.config['JiShuL']
+            baseh = config_argc.config['JiShuH']
+            lock = Lock()
+            Process(target=putdata,arg=('-d',)).start()
+            Process(target=comp_func,arg=(soinsur,basel,baseh,lock)).start()
+            Process(target=outfile,arg=('-o')).start()
+        else:  
+            raise "Parameter Error"
+    except:
+        print("Parameter Error")
    
-    def outfile():
-        datalist = que2.get() 
         
    
 
